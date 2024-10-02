1bf6ed56ba2a51e280b3a6366a64a13ee145fa7f
Mohammed Yahya.S <mohammedyahya.at@gmail.com>
2024-10-02 00:47:46 -0700

    Add files via upload

---
+++ swap1.c
@@ -0,0 +1,16 @@
+//swapping two values using arithmetic operators
+#include <stdio.h>
+int main() {
+    
+    int a,b;
+    printf("Enter values for a and b");
+    scanf("%d%d",&a,&b);
+    
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    
+    printf("new value of a and b is %d,%d",a,b);
+    
+    return 0;
+}
